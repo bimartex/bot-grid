28be85e9d926a082f32057b0da3e8a6d268c10f8
bimartex <bimartexpress@gmail.com>
2025-05-22 06:49:49 -0700

    Add files via upload

---
+++ client/src/App.tsx
@@ -0,0 +1,38 @@
+import { Switch, Route } from "wouter";
+import { queryClient } from "./lib/queryClient";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "@/components/ui/toaster";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import NotFound from "@/pages/not-found";
+import Layout from "./components/Layout";
+import Dashboard from "./pages/Dashboard";
+import History from "./pages/History";
+import Assets from "./pages/Assets";
+import Settings from "./pages/Settings";
+
+function Router() {
+  return (
+    <Switch>
+      <Route path="/" component={Dashboard} />
+      <Route path="/assets" component={Assets} />
+      <Route path="/history" component={History} />
+      <Route path="/settings" component={Settings} />
+      <Route component={NotFound} />
+    </Switch>
+  );
+}
+
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+        <Toaster />
+        <Layout>
+          <Router />
+        </Layout>
+      </TooltipProvider>
+    </QueryClientProvider>
+  );
+}
+
+export default App;
